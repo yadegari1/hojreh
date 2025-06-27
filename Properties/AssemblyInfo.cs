746a927f140da7305f68b6c99ab4e31a461e15f3
yadegari <144546516+yadegari1@users.noreply.github.com>
2025-06-26 18:26:38 -0700

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.4.0.4")]
+[assembly: CompilationRelaxations(8)]
+[assembly: Guid("464a9406-fc12-4de2-a453-3f3ee30f0d7e")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("نرم افزار حسابداری میوه و تره بارحجره")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyDescription("نرم افزار حسابداری میوه و تره بار حجره")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("نرم افزار حسابداری میوه و تره بار حجره")]
+[assembly: AssemblyCopyright("مهندس یادگاری - همراه : 09019024221")]
+[assembly: AssemblyCompany("")]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyFileVersion("1.4.0.4")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
